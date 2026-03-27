93ca9facc6e9455159fd736e2e5821c2e52dd241
RezkyKam50 <rezkykam34@gmail.com>
2026-03-27 15:30:44 +0700

    update

--- exp/train_epoch100.ps1
+++ exp/train_epoch100.ps1
@@ -1,6 +1,6 @@
 # python exp/multimodal_trainer.py --loss_func tversky --epochs 100 --batch_size 6 --torch_seed 124
 
-python exp/multimodal_trainer.py --loss_func tversky --epochs 75 --batch_size 6  
+python exp/multimodal_trainer.py --loss_func tversky --epochs 1 --batch_size 6  
 
 # python exp/multimodal_trainer.py --loss_func focal --epochs 20 --batch_size 6  
 
