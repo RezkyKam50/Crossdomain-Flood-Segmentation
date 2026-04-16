9f1c6e0c079539cd2b8400928b0f19d73b89af6b
RezkyKam50 <rezkykam34@gmail.com>
2026-04-16 16:59:28 +0700

    up

--- exp/multimodal_trainer.py
+++ exp/multimodal_trainer.py
@@ -428,8 +428,8 @@ def train(model, model_name, train_loader, valid_loader, test_loader, bolivia_lo
     #     model.change_s1_trainability(False) # Freeze S1, update S2
     #     model.change_s2_trainability(True)
     #     logger.info(f"Module S1 frozen. Trainable parameters: {get_number_of_trainable_parameters(model):,}")
-    # num_params_phase_2 = ph_loop(model, train_loader, valid_loader, criterion, device, writer, scheduler, optimizer, model_dir, args)
-    # torch.cuda.empty_cache()
+    num_params_phase_2 = ph_loop(model, train_loader, valid_loader, criterion, device, writer, scheduler, optimizer, model_dir, args)
+    torch.cuda.empty_cache()
 
     # FT Phase 3
     # if model_name in three_phase_model and args.finetune_ratio is not None:
@@ -437,8 +437,8 @@ def train(model, model_name, train_loader, valid_loader, test_loader, bolivia_lo
     #     model.change_s2_trainability(True)
     #     # model.change_prithvi_trainability(True) # Unfreeze Prithvi
     #     logger.info(f"All weights unfrozen. Trainable parameters: {get_number_of_trainable_parameters(model):,}")
-    # num_params_phase_ft = ft_loop(model, model_name, train_loader, valid_loader, criterion, device, writer, scheduler, optimizer, args)
-            #model, model_name, train_loader, valid_loader, criterion, device, writer, scheduler, optimizer, args
+    num_params_phase_ft = ft_loop(model, model_name, train_loader, valid_loader, criterion, device, writer, scheduler, optimizer, args)
+        #model, model_name, train_loader, valid_loader, criterion, device, writer, scheduler, optimizer, args
 
 
     # FT Phase Prithvi
